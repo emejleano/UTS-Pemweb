a4d3c91aaa20a42e3738d182bd093c7d9aa8b159
emejleano <lanodoang03@gmail.com>
2024-10-20 21:42:17 +0700

    perubahan dasboard

--- dashboard.php
+++ dashboard.php
@@ -85,6 +85,40 @@ if (!isset($_SESSION['username'])) {
                 </div>
             </div>
         </div>
+
+        <div class="col-md-4">
+            <div class="card">
+                <img src="https://i.pinimg.com/564x/61/f4/44/61f444585217e77e329834a367f18b85.jpg" class="card-img-top blog-image" alt="Blog Image">
+                <div class="card-body">
+                    <h5 class="card-title">Labuan Bajo</h5>
+                    <p class="card-text">Labuan Bajo merupakan salah satu kelurahan yang berada di Kecamatan Komodo, Kabupaten Manggarai Barat, Provinsi Nusa Tenggara Timur, Indonesia. Labuan Bajo juga merupakan pusat pemerintahan dari Kecamatan Komodo dan sekaligus merupakan ibu kota Kabupaten Manggarai Barat.</p>
+                    <a href="https://id.wikipedia.org/wiki/Labuan_Bajo,_Komodo,_Manggarai_Barat" class="btn btn-primary">Baca Selengkapnya</a>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-4">
+            <div class="card">
+                <img src="https://i.pinimg.com/736x/87/0b/9a/870b9a61ed69114b434ddd06cdd35141.jpg" class="card-img-top blog-image" alt="Blog Image">
+                <div class="card-body">
+                    <h5 class="card-title">Yogyakarta</h5>
+                    <p class="card-text">Kota Yogyakarta adalah ibu kota sekaligus pusat pemerintahan dan perekonomian dari provinsi Daerah Istimewa Yogyakarta, Indonesia. Kota ini adalah kota yang mempertahankan konsep tradisional dan budaya Jawa.</p>
+                    <a href="https://id.wikipedia.org/wiki/Kota_Yogyakarta" class="btn btn-primary">Baca Selengkapnya</a>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-4">
+            <div class="card">
+                <img src="https://i.pinimg.com/736x/b4/4c/f1/b44cf19a3923d1f543edee354450ec98.jpg" class="card-img-top blog-image" alt="Blog Image">
+                <div class="card-body">
+                    <h5 class="card-title">Jayapura</h5>
+                    <p class="card-text">Kota Jayapura adalah sebuah kota sekaligus menjadi ibu kota provinsi Papua, Indonesia. Kota ini juga merupakan ibu kota provinsi yang terletak paling timur di Indonesia, dan berbatasan langsung dengan negara Papua Nugini, yang terletak di Teluk Jayapura. Kota ini didirikan oleh Kapten Infanteri F.J.P. Sachse dari kerajaan Belanda pada 7 Maret 1910. Dari tahun 1910 ke 1962, kota ini dikenal sebagai Hollandia merupakan ibu kota distrik dengan nama yang sama di timur laut Pulau Papua bagian barat.</p>
+                    <a href="https://id.wikipedia.org/wiki/Kota_Jayapura" class="btn btn-primary">Baca Selengkapnya</a>
+                </div>
+            </div>
+        </div>
+
         <div class="col-md-4">
             <div class="card">
                 <img src="https://i.pinimg.com/736x/1b/88/94/1b889486392a14ae4f16d282d815bf18.jpg" class="card-img-top blog-image" alt="Blog Image">
